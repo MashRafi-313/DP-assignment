b3b8f94de4ab715b75b2e506989d125bba318092
Mohammad Tusher Imran Mashrafi <122280570+MashRafi-313@users.noreply.github.com>
2024-05-16 00:36:44 +0600

    Create File.java

---
+++ composite_pattern/File.java
@@ -0,0 +1,15 @@
+public class File implements Directory{
+    private int size;
+
+    public File(int size) {
+        this.size = size;
+    }
+
+
+    public int getFileSize() {
+        return size;
+    }
+
+    
+
+}
