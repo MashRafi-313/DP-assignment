2db394c0c25bcdb6e264d8660c1668379fad5d4d
Mohammad Tusher Imran Mashrafi <122280570+MashRafi-313@users.noreply.github.com>
2024-05-19 23:28:12 +0600

    Delete BuilderPattern directory

--- BuilderPattern/Client.java
+++
@@ -1,7 +0,0 @@
-public class Client {
-    public static void main(String[] args) {
-        Phone phone;
-        phone = new PhoneBuilder().setOs("Android").setRam(2).getPhone();
-        phone.getResult();
-    }
-}
